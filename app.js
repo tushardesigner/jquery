c73b4b47125c0735cb09d5ba3530bdc97322a322
tushardesigner <tusharmajumdar1110@gmail.com>
2025-03-29 13:56:26 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
+let version = Date.now();
+let link = document.createElement("link")
+link.rel = "stylesheet"
+link.href = `./style.css?ver=${version}`
+document.head.appendChild(link)
+console.log(link)
+
+
+
 jQuery(document).ready(function($){
     $("button").click(function(){
     alert("Hello")
